ca1c86c5ba8dc9ee523f8424cc95e8d3276c6c65
najamalamkhan1 <najamalamkhan1321@gmail.com>
2026-04-10 18:24:25 +0500

    upadte delete functionality 1st try

--- Routes/storeRoute.js
+++ Routes/storeRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Store = require("../Models/store");
+const mongoose = require('mongoose')
 
 
 // ========================================
