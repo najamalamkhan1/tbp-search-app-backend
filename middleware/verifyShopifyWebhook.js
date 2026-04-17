6000529649b8f0854c0f4ff40ae071cd154fb504
najamalamkhan1 <najamalamkhan1321@gmail.com>
2026-04-17 17:57:13 +0500

    webhooks update 3rd try

--- middleware/verifyShopifyWebhook.js
+++ middleware/verifyShopifyWebhook.js
@@ -1,28 +1,20 @@
 const crypto = require("crypto");
 
 const verifyShopifyWebhook = (req, res, next) => {
-  try {
-    const hmac = req.headers["x-shopify-hmac-sha256"];
+  const hmac = req.headers["x-shopify-hmac-sha256"];
 
-    if (!process.env.SHOPIFY_WEBHOOK_SECRET) {
-      return res.status(500).send("Secret missing ❌");
-    }
+  const hash = crypto
+    .createHmac("sha256", process.env.SHOPIFY_WEBHOOK_SECRET)
+    .update(JSON.stringify(req.body)) // ✅ req.body must be Buffer
+    .digest("base64");
 
-    const hash = crypto
-      .createHmac("sha256", process.env.SHOPIFY_WEBHOOK_SECRET)
-      .update(req.body) // ✅ IMPORTANT (no stringify, no 'body')
-      .digest("base64");
-
-    if (hash !== hmac) {
-      return res.status(401).send("Webhook verification failed ❌");
-    }
-
-    next();
-
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Webhook error");
+  if (hash !== hmac) {
+    return res.status(401).send("Webhook verification failed");
   }
+
+  next();
+  console.log("TYPE:", typeof req.body);
+  console.log("IS BUFFER:", Buffer.isBuffer(req.body));
 };
 
 module.exports = verifyShopifyWebhook;
