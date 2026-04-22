e3ce974c5df2a736008f1c08fd9ecdfe07fe44b3
najamalamkhan1 <najamalamkhan1321@gmail.com>
2026-04-23 01:39:15 +0500

    cors update

--- server.js
+++ server.js
@@ -7,26 +7,30 @@ const webhooks = require('./Routes/webhookRoutes');
 const app = express();
 
 app.use("/webhooks", express.raw({ type: "application/json" }));
-const allowedOrigins = [
-  "https://admin.shopify.com",
-  "https://clarke-exactly-detect-sectors.trycloudflare.com"
-];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-
-    if (
-      allowedOrigins.includes(origin) ||
-      origin.endsWith(".myshopify.com")
-    ) {
-      return callback(null, true);
-    }
-
-    return callback(new Error("Not allowed by CORS"));
-  },
+  origin: true,
   credentials: true
 }));
+// const allowedOrigins = [
+//   "https://admin.shopify.com",
+//   "https://clarke-exactly-detect-sectors.trycloudflare.com"
+// ];
+
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin) return callback(null, true);
+
+//     if (
+//       allowedOrigins.includes(origin) ||
+//       origin.endsWith(".myshopify.com")
+//     ) {
+//       return callback(null, true);
+//     }
+
+//     return callback(new Error("Not allowed by CORS"));
+//   },
+//   credentials: true
+// }));
 
 // MongoDB Connection
 mongoose.connect(
